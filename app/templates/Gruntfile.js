0a13e4aef717e8c79b78010e3abbfeab71343bfe
Benjamin Wasilewski <benwasilewski@gmail.com>
2013-12-25 13:21:33 -0600

    adding generated gruntfile

---
+++ app/templates/Gruntfile.js
@@ -0,0 +1,38 @@
+'use strict';
+
+module.exports = function (grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        // Task configuration.
+        jshint: {
+            options: {
+                jshintrc: true
+            },
+            gruntfile: {
+                src: 'Gruntfile.js'
+            },
+            app: {
+                src: ['app/js/**/*.js']
+            }
+        },
+        watch: {
+            gruntfile: {
+                files: '<%= jshint.gruntfile.src %>',
+                tasks: ['jshint:gruntfile']
+            },
+            app: {
+                files: '<%= jshint.app.src %>',
+                tasks: ['jshint:app']
+            }
+        }
+    });
+
+    // These plugins provide necessary tasks.
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    // Default task.
+    grunt.registerTask('default', ['jshint', 'watch']);
+
+};
