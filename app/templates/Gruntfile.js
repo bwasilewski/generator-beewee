fffb958fc745383f8f6e309d8a3e3f60feb2d7a5
Benjamin Wasilewski <benwasilewski@gmail.com>
2013-12-25 16:04:25 -0600

    updating generated gruntfile and dependencies

--- app/templates/Gruntfile.js
+++ app/templates/Gruntfile.js
@@ -2,37 +2,40 @@
 
 module.exports = function (grunt) {
 
-    // Project configuration.
-    grunt.initConfig({
-        // Task configuration.
-        jshint: {
-            options: {
-                jshintrc: true
-            },
-            gruntfile: {
-                src: 'Gruntfile.js'
-            },
-            app: {
-                src: ['app/js/**/*.js']
-            }
-        },
-        watch: {
-            gruntfile: {
-                files: '<%= jshint.gruntfile.src %>',
-                tasks: ['jshint:gruntfile']
-            },
-            app: {
-                files: '<%= jshint.app.src %>',
-                tasks: ['jshint:app']
-            }
-        }
-    });
+  // Project configuration.
+  grunt.initConfig({
+    // Task configuration.
+    jshint: {
+      options: {
+        jshintrc: true
+      }
+     ,gruntfile: {
+        src: 'Gruntfile.js'
+      }
+     ,app: {
+        src: ['app/js/**/*.js']
+      }
+    }
 
-    // These plugins provide necessary tasks.
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-watch');
+   ,watch: {
 
-    // Default task.
-    grunt.registerTask('default', ['jshint', 'watch']);
+      gruntfile: {
+        files: '<%= jshint.gruntfile.src %>'
+       ,tasks: ['jshint:gruntfile']
+      }
+
+     ,app: {
+        files: '<%= jshint.app.src %>'
+       ,tasks: ['jshint:app']
+      }
+    }
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Default task.
+  grunt.registerTask('default', ['jshint', 'watch']);
 
 };
