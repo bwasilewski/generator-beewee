ca27cc59cfdc10057d1a8ce4ca1c7995233ca0be
Benjamin Wasilewski <benwasilewski@gmail.com>
2013-12-25 16:03:41 -0600

    add copy directive to gruntfile and set it to copy inuit.css from bower_components

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,37 +2,53 @@
 
 module.exports = function (grunt) {
 
-    // Project configuration.
-    grunt.initConfig({
-        // Task configuration.
-        jshint: {
-            options: {
-                jshintrc: true
-            },
-            gruntfile: {
-                src: 'Gruntfile.js'
-            },
-            generator: {
-                src: 'app/templates/*.js'
-            }
-        },
-        watch: {
-            gruntfile: {
-                files: '<%= jshint.gruntfile.src %>',
-                tasks: ['jshint:gruntfile']
-            },
-            generator: {
-                files: '<%= jshint.generator.src %>',
-                tasks: ['jshint:generator']
-            }
-        }
-    });
+  // Project configuration.
+  grunt.initConfig({
+    // Task configuration.
+    copy: {
+      app: {
+        expand: true
+       ,cwd: 'bower_components/'
+       ,src: 'inuit.css/**'
+       ,dest: 'app/templates/'
+      }
+    }
 
-    // These plugins provide necessary tasks.
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-watch');
+   ,jshint: {
 
-    // Default task.
-    grunt.registerTask('default', ['jshint', 'watch']);
+      options: {
+        jshintrc: true
+      }
+
+     ,gruntfile: {
+        src: 'Gruntfile.js'
+      }
+
+     ,generator: {
+        src: 'app/templates/*.js'
+      }
+    }
+
+   ,watch: {
+
+      gruntfile: {
+        files: '<%= jshint.gruntfile.src %>'
+       ,tasks: ['jshint:gruntfile']
+      }
+
+     ,generator: {
+        files: '<%= jshint.generator.src %>'
+       ,tasks: ['jshint:generator']
+      }
+    }
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+
+  // Default task.
+  grunt.registerTask('default', ['jshint', 'watch']);
 
 };
